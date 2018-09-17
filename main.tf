352db9ea9629c00834288bb8edc52e355cae27a2
Juan Sanchez <juan.sanchez1192@gmail.com>
2018-09-17 16:02:24 +0200

    Add VPC base configuration

--- main.tf
+++ main.tf
@@ -6,4 +6,24 @@ terraform {
 
 provider "aws" {
   version = "~> 1.36.0"
+  region  = "${var.region}"
+  profile = "${var.profile}"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  create_vpc = "${var.vpc-create}"
+
+  name = "${var.vpc-name}"
+  cidr = "${var.vpc-cidr}"
+  azs  = "${local.vpc-azs}"
+
+  public_subnets = "${var.vpc-public-subnets}"
+
+  # Every instance deployed within the VPC will get a hostname
+  enable_dns_hostnames = true
+
+  # Every instance will have a dedicated internal endpoint to communicate with S3
+  enable_s3_endpoint = true
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,48 @@
 # Variables file
 
+## Provider variables
+variable "region" {
+  description = "AWS region where the entire infrastructure is deployed"
+  default     = "us-east-1"
+}
+
+variable "profile" {
+  description = "AWS account profile to use for deployments"
+}
+
+## VPC variables
+
+variable "vpc-region-azs" {
+  default = {
+    # N. Virginia
+    "us-east-1" = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+    # Ireland
+    "eu-west-1" = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  }
+}
+
+variable "vpc-create" {
+  description = "Whether or not create a vpc using this module. If you have a preexisting VPC, just mark it as false"
+  default     = true
+}
+
+variable "vpc-name" {
+  description = "Name to be used on all the resources as identifier"
+}
+
+variable "vpc-cidr" {
+  default = "10.0.0.0/16"
+}
+
+locals {
+  # Here are selected the default AZs used for the subnets  # By default, a, b and c AZs are chosen and only supports  # US-EAST-1 🇺🇸 and EU-WEST-1 🇪🇺 regions, for now.
+
+  # 👎 We need to use locals becuase variables do not support interpolation
+  vpc-azs = "${var.vpc-region-azs["${var.region}"]}"
+}
+
+variable "vpc-public-subnets" {
+  description = "List of subnets to be used for the vpc"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
